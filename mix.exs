1d73b437ce797857a25d2803f4e9e7b7bd495d99
Benjamin Milde <benni@kobrakai.de>
2025-08-22 17:03:12 +0200

    Release 0.3.3

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule PhoenixVite.MixProject do
       name: "Phoenix Vite",
       source_url: "https://github.com/LostKobrakai/phoenix_vite",
       app: :phoenix_vite,
-      version: "0.3.2",
+      version: "0.3.3",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
